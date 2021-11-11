8141affdbff7a496241667eddec76719d65173e9
chtulur <molnar.gly@gmail.com>
2021-11-11 17:55:07 +0100

    Done

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,17 @@
+const HU = {
+  date(sth) {
+    return new Intl.DateTimeFormat("hu-HU").format(sth);
+  },
+  curreny(amount) {
+    return new Intl.NumberFormat("hu-HU", {
+      style: "currency",
+      currency: "HUF",
+    }).format(amount);
+  },
+  list(arr) {
+    let lastGuy = arr.pop();
+    return `${arr.join(", ")} és ${lastGuy}`;
+  },
+};
 
-
-
-
-
-
-// export default HU
+export default HU;
